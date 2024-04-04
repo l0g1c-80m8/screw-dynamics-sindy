40a366751859e02b17743dfbbd30f09fb3005f88
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-04-04 16:26:38 -0700

    define skeleton of the main file

--- src/main.py
+++ src/main.py
@@ -1,7 +1,33 @@
+import argparse
 import sys
 
+from src.dataloader import ScrewdrivingDataset
+
+
+def get_args():
+    # define parser
+    parser = argparse.ArgumentParser()
+
+    # set args
+    parser.add_argument('--data_dir', type=str, default='./data',
+                        action='store', dest='data_dir', help='data directory')
+    parser.add_argument('--file_ext', type=str, default='csv',
+                        action='store', dest='file_ext', help='data directory')
+
+    # return parsed arguments
+    return parser.parse_args()
+
+
+def main():
+    ScrewdrivingDataset(**vars(ARGS))
+
+
 if __name__ == '__main__':
-    print('hello, friend')
+    # get args
+    ARGS = get_args()
+
+    # exec
+    main()
 
     # exit without errors
     sys.exit()
