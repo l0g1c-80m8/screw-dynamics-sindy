4410777c4f7fecb91f5a4d0e711f320a7ff08eb8
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-04-08 16:00:19 -0700

    bugfix - don't pass non-required args to nn.Module

--- src/model.py
+++ src/model.py
@@ -9,7 +9,7 @@ class SindyModel(nn.Module):
     _COFF_KEY = 'sindy_coefficients'
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        super().__init__()
 
         self._params = Namespace(**kwargs)
         self._library_dim = None
