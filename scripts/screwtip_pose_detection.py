cd42177d803ebc5a4938c7619a71b5bf62a3560b
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-04-25 18:42:27 -0700

    update filter and boundary maps for 8_30_8_M6_1500

--- scripts/screwtip_pose_detection.py
+++ scripts/screwtip_pose_detection.py
@@ -51,6 +51,9 @@ def get_pixel(filepath, subdir):
         '4_30_4_M5_500': [
             {"lower": np.array([130, 30, 80]), "upper": np.array([179, 255, 155])}
         ],
+        '8_30_8_M6_1500': [
+            {"lower": np.array([130, 30, 80]), "upper": np.array([179, 255, 155])}
+        ],
     }
 
     boundary_map = {
@@ -62,6 +65,7 @@ def get_pixel(filepath, subdir):
         '2_30_2_M4_1500': ((290, 200), (350, 260)),
         '4_30_4_M5_500': ((290, 200), (350, 260)),
         '23_90_5_M5_1500': ((340, 200), (420, 260)),
+        '8_30_8_M6_1500': ((290, 200), (360, 260)),
     }
 
     combined_mask = np.zeros_like(image[:, :, 0])
