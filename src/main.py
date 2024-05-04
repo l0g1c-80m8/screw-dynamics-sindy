aa31592d4b0f8c1fa93f28ef919011c488c75cdc
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-05-03 21:56:43 -0700

    update learning rate

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ def get_args():
                         action='store', dest='state_var_dim', help='dimension of state variable')
     parser.add_argument('--device', type=str, default='cpu',
                         action='store', dest='device', help='device to run operations on')
-    parser.add_argument('--learning_rate', type=float, default=.01,
+    parser.add_argument('--learning_rate', type=float, default=.0001,
                         action='store', dest='learning_rate', help='learning rate for training')
     parser.add_argument('--weight_decay', type=float, default=.000001,
                         action='store', dest='weight_decay', help='weight decay for training')
