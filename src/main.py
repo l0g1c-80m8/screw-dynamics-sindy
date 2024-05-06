5908f90ce93c68078391c5d9eee98cccf07b778d
Rutvik Patel <rutvik.p4t3l@gmail.com>
2024-05-06 15:06:27 -0700

    ADD MORE ARGS

--- src/main.py
+++ src/main.py
@@ -33,8 +33,10 @@ def get_args():
                         action='store', dest='learning_rate', help='learning rate for training')
     parser.add_argument('--weight_decay', type=float, default=.000001,
                         action='store', dest='weight_decay', help='weight decay for training')
-    parser.add_argument('--epochs', type=int, default=500,
+    parser.add_argument('--epochs', type=int, default=50,
                         action='store', dest='epochs', help='epochs for training')
+    parser.add_argument('--window_length', type=int, default=200,
+                        action='store', dest='window_length', help='batch window size')
 
     # return parsed arguments
     return parser.parse_args()
